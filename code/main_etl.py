2121f060305a679b4c7d2d7e0ee4bc464370d71e
jbarbour3 <john.matt.barbour@gmail.com>
2019-07-14 20:39:12 -0400

    Initial Commit

---
+++ code/main_etl.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+# download dealership listings
+# note: used auto dealers can be found at
+# #http://verify.sos.ga.gov/verification/Search.aspx?facility=Y&SubmitComplaint=Y
+# official toyota.com web results can be found at
+# https://www.toyota.com/dealers/#search&zipcode=30909 by entering the zip code
+# results returned as responsive view from webapp, may require headless client to parse
+# Atl area returns 17 sites, many with variable website structure.
+
+
+# save all raw results to disk
+
+# run etl process
+
+## extract all relevant data (pics, metadata, text, etc.) from all listings.
+
+## transform/tag/optimize data accordingly.
+# dealers should be an object with corresponding schema, stats, etc...
+
+## load all data into database/tables etc.
