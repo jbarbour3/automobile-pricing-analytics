dfced802677f3b504e4c92e7f5b1ffee715609de
jbarbour3 <john.matt.barbour@gmail.com>
2019-11-24 23:59:04 -0500

    Added the R project directory

---
+++ r_work/generic_analytics_workflow.R
@@ -0,0 +1,20 @@
+# this script gives a generic workflow for the following
+# 1. performing data analysis in R (cleaning up data, prepping it for plotting/modeling)
+# 2. descriptive analytics with ggplot
+# 3. running some analytics models and visualizing those results
+
+
+# L import libraries and load constants
+
+library(ggplot2)
+data_file = 
+
+
+
+
+# Load raw_data
+
+raw_data = read.csv(data_file)
+
+
+
